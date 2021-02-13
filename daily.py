7cb51e377cb5dbebeb784d13f69c2c361855edcd
Abdias Labrador LAptop windows10 asus <abdiaslabrador@gmail.com>
2021-02-12 21:44:07 -0400

    practica revert

---
+++ daily.py
@@ -0,0 +1,10 @@
+import os
+import random
+from subprocess import call
+
+ar = [(259, 'NO CHANCE'), (258, 'EN CLASE'), (253, 'FINALIZADO'),
+      (257, 'EN CLASE'), (255, 'NO CHANCE'), (256, 'FINALIZADO'),
+      (254, 'FINALIZADO'), (252, 'EN CLASE'), (251, 'NO CHANCE')]
+
+ar = list(filter(lambda x: x[1] == 'FINALIZADO', ar))
+print(ar)
